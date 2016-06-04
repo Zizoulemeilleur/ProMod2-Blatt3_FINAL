d3f6e5a767379391fdf22eeacfa620cda8f9637f
Anton Stepan <info@antonstepan.it>
2016-06-04 18:51:51 +0200

    init

--- ProMod2 Blatt3_FINAL/src/team/sheldon/server/ClientConnectionThread.java
+++ ProMod2 Blatt3_FINAL/src/team/sheldon/server/ClientConnectionThread.java
@@ -13,6 +13,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
 import java.net.Socket;
+import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,29 +25,28 @@ import team.sheldon.person.Person;
  */
 public class ClientConnectionThread extends Thread {
 
-	private Socket socket;
-	private Logger logger;
+	private final Socket socket;
+	private final Logger logger;
 
 	private ObjectInputStream objectInputStream;
 	private ObjectOutputStream objectOutputStream;
+	private final String filename;
 
-	public ClientConnectionThread(Socket socket, Logger logger) {
+	public ClientConnectionThread(Socket socket, Logger logger, String filename) {
 		this.socket = socket;
 		this.logger = logger;
+		this.filename = filename;
 	}
 
 	@Override
 	public void run() {
 
 		try {
-			InputStreamReader inputStream = new InputStreamReader(
-					socket.getInputStream());
+			final InputStreamReader inputStream = new InputStreamReader(socket.getInputStream());
 			final BufferedReader in = new BufferedReader(inputStream);
-			final BufferedWriter out = new BufferedWriter(
-					new OutputStreamWriter(socket.getOutputStream()));
+			final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-			objectOutputStream = new ObjectOutputStream(
-					socket.getOutputStream());
+			objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
 			objectInputStream = new ObjectInputStream(socket.getInputStream());
 
 			String s;
@@ -56,18 +56,20 @@ public class ClientConnectionThread extends Thread {
 					handleMessage(s, socket, out);
 				}
 			}
-		} catch (Exception e) {
+		} catch (final SocketException e) {
+			// Nothing to do here; we closed the socket.
+		} catch (final Exception e) {
 			logger.log(socket, "Fehler in handle message" + e.getMessage());
 			e.printStackTrace();
 		}
-		logger.log(socket,
-				"<-- LocalAddress abgemeldet: " + socket.getLocalAddress());
-		logger.log(socket,
-				"<-- InetAddress abgemeldet: " + socket.getInetAddress());
+
+		ServerGui.decrementClients();
+		logger.log(socket, "<-- LocalAddress abgemeldet: " + socket.getLocalAddress());
+		logger.log(socket, "<-- InetAddress abgemeldet: " + socket.getInetAddress());
 	}
 
-	private void handleMessage(String message, Socket socket,
-			BufferedWriter ausgang) throws IOException, ClassNotFoundException {
+	private void handleMessage(String message, Socket socket, BufferedWriter ausgang)
+			throws IOException, ClassNotFoundException {
 		switch (message) {
 		case "1":
 			logger.log(socket, "1");
@@ -79,25 +81,22 @@ public class ClientConnectionThread extends Thread {
 		case "3":
 			logger.log(socket, "3");
 			@SuppressWarnings("unchecked")
-			List<Person> personen = (List<Person>) objectInputStream
-					.readObject();
+			final List<Person> personen = (List<Person>) objectInputStream.readObject();
 			// Cast (Zaubert das Object in eine Liste von Prs
-			logger.log(socket, "Personen vom Client bekommen. Anzahl: "
-					+ personen.size());
+			logger.log(socket, "Personen vom Client bekommen. Anzahl: " + personen.size());
 			save(personen, socket);
 
 			break;
 
 		case "4":
 			logger.log(socket, "4");
-			List<Person> geladenePersonen = laden(socket);
+			final List<Person> geladenePersonen = laden(socket);
 
 			objectOutputStream.flush();
 			objectOutputStream.writeObject(geladenePersonen);
 			objectOutputStream.flush(); // flushd (wie beim stuff das gewritete
 			// "durch"
-			logger.log(socket, "Schicke Personen an den Client. Anzahl: "
-					+ geladenePersonen.size());
+			logger.log(socket, "Schicke Personen an den Client. Anzahl: " + geladenePersonen.size());
 			logger.log(socket, "Flushing stream ...");
 			// objectPost.close();
 			break;
@@ -108,8 +107,6 @@ public class ClientConnectionThread extends Thread {
 
 		case "6":
 			logger.log(socket, "6");
-			String filename = (String) objectInputStream.readObject();
-			logger.log(socket, "Zu loeschende Datei: " + filename);
 			deleteFile(filename);
 			break;
 
@@ -127,16 +124,16 @@ public class ClientConnectionThread extends Thread {
 
 	private final void save(List<Person> personen, Socket socket) {
 		try {
-			File f = new File("adreli.csv");
+			final File f = new File(filename);
 			if (!f.exists()) {
 				logger.log(socket, "Datei existiert nicht, wird erstellt.");
 				f.createNewFile();
 			}
 		}
 
-		catch (Exception e) {
+		catch (final Exception e) {
 			logger.log(socket, "Fehler: " + e.getMessage());
-		}// Ende Datei erstellen
+		} // Ende Datei erstellen
 
 		// In Datei schreiben
 		try {
@@ -146,16 +143,14 @@ public class ClientConnectionThread extends Thread {
 			 * Alle Daten (bzw. Personen) der ArraysList in die Datei
 			 * untereinander speichern
 			 */
-			for (Person p : personen) {
+			for (final Person p : personen) {
 				datei = new FileWriter("adreli.csv", true);
-				datei.write(p.getName() + ";" + p.getVname() + ";"
-						+ p.getAnrede() + ";" + p.getStrasse() + ";"
-						+ p.getPlz() + ";" + p.getOrt() + ";" + p.getTelefon()
-						+ ";" + p.getFax() + ";" + p.getBem());
+				datei.write(p.getName() + ";" + p.getVname() + ";" + p.getAnrede() + ";" + p.getStrasse() + ";"
+						+ p.getPlz() + ";" + p.getOrt() + ";" + p.getTelefon() + ";" + p.getFax() + ";" + p.getBem());
 				datei.append(System.getProperty("line.separator"));
 				datei.close();// Writer-Stream wieder schliessen
 			}
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.log(socket, "Fehler: " + e.getMessage());
 		}
 
@@ -166,28 +161,28 @@ public class ClientConnectionThread extends Thread {
 
 	private final List<Person> laden(Socket socket) {
 		logger.log(socket, "Personen werden geladen ...");
-		List<Person> daten = new ArrayList<Person>();
+		final List<Person> daten = new ArrayList<Person>();
 		// ArrayList wird gefüllt
 
 		try {
 			// eindim. Array deklarieren als Zwischenspeicher
 			String[] liste = null;
 
-			FileReader fr = new FileReader("adreli.csv");
-			BufferedReader br = new BufferedReader(fr);
+			final FileReader fr = new FileReader("adreli.csv");
+			final BufferedReader br = new BufferedReader(fr);
 
 			String zeile = null;
 			while ((zeile = br.readLine()) != null) {
 				liste = zeile.split(";");
-				Person p = new Person(liste[0], liste[1], liste[2], liste[3],
-						liste[4], liste[5], liste[6], liste[7], liste[8]);
+				final Person p = new Person(liste[0], liste[1], liste[2], liste[3], liste[4], liste[5], liste[6],
+						liste[7], liste[8]);
 				daten.add(p);
 			}
 
 			fr.close();
 			br.close();
 
-		} catch (Exception e) {
+		} catch (final Exception e) {
 			logger.log(socket, "Fehler: " + e.getMessage());
 		}
 		logger.log(socket, "Personen geladen, Anzahl: " + daten.size());
@@ -197,12 +192,11 @@ public class ClientConnectionThread extends Thread {
 
 	private void deleteFile(String filename) {
 		// TODO Exception Handling
-		File file = new File(filename);
+		final File file = new File(filename);
 		if (file.delete()) {
 			System.out.println("Datei: " + filename + " wurde geloescht.");
 		} else {
-			System.out.println("Loeschen der Datei " + filename
-					+ " hat leider nicht geklappt.");
+			System.out.println("Loeschen der Datei " + filename + " hat leider nicht geklappt.");
 		}
 	}
 
